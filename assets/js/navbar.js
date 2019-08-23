77e5f4e892e7cb6d93c492ff454a53967bf638d4
JPCadarn <mariotti660@gmail.com>
2019-08-23 16:39:08 -0300

    navbar e produtos

---
+++ assets/js/navbar.js
@@ -0,0 +1,10 @@
+function openNav() {
+	document.getElementById("menuLateral").style.width = "250px";
+	document.getElementById("main").style.marginLeft = "250px";
+	document.body.style.opacity = "40";
+  }
+
+function closeNav() {
+	document.getElementById("menuLateral").style.width = "0";
+	document.getElementById("main").style.marginLeft = "0";
+}
