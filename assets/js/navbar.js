e35b5a433d74dd43b6919d0149616011b407413a
JPCadarn <mariotti660@gmail.com>
2019-09-04 11:23:03 -0300

    detalhes

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,10 +1,18 @@
 function openNav() {
-	document.getElementById("menuLateral").style.width = "15.625%";
-	document.getElementById("main").style.marginLeft = "15.625%";
-	document.body.style.opacity = "40";
+	var tamanhoNav;
+	if (self.innerWidth <= 1000) 
+		tamanhoNav = "40%";
+	else
+		tamanhoNav = "270px";
+	document.getElementById("menuLateral").style.width = tamanhoNav;
+	document.getElementById("menuLateral").style.boxShadow = "4px 4px 100px 4px rgba(149, 214, 0, 0.71)";
+	document.getElementById("menuLateral").style.webkitBoxShadow = "4px 4px 100px 4px rgba(149, 214, 0, 0.71)";
+	document.getElementById("nav").style.boxShadow = "4px 4px 100px 4px rgba(0, 0, 0, 0.71)";
+	document.getElementById("nav").style.webkitBoxShadow = "4px 4px 100px 4px rgba(0, 0, 0, 0.71)";
   }
 
 function closeNav() {
 	document.getElementById("menuLateral").style.width = "0";
-	document.getElementById("main").style.marginLeft = "0";
+	document.getElementById("nav").style.boxShadow = "4px 4px 100px 4px rgba(149, 214, 0, 0.71)";
+	document.getElementById("nav").style.webkitBoxShadow = "4px 4px 100px 4px rgba(149, 214, 0, 0.71)";
 }
